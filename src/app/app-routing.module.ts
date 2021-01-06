72d39776974f647240d783225130422e359002c4
Subash.B <Subash.Bose@contractor.tranetechnologies.com>
2021-01-06 22:04:44 +0530

    adding carosal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 // components
 import { LogInSignUpComponent } from './header/log-in-sign-up/log-in-sign-up.component';
+import { HomePageComponent } from './home-page/home-page.component';
 const routes: Routes = [
+  { path: '', component: HomePageComponent },
   { path: 'login', component: LogInSignUpComponent }
 
 ];
